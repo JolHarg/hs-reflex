7e5cd4c565df93284c0a40291b6d4cbfb3567582
Dan Dart <git@dandart.co.uk>
2023-01-03 20:32:02 +0000

    a bit of space on the register box

--- lib/UI/Tab/Register.hs
+++ lib/UI/Tab/Register.hs
@@ -12,16 +12,16 @@ import           UI.Bootstrap.Button
 import           UI.Bootstrap.Form
 import           UI.Bootstrap.Pane
 
-widgetRegister ∷ (MonadWidget t m) ⇒ m (Event t (Maybe User))
+widgetRegister ∷ MonadWidget t m ⇒ m (Event t (Maybe User))
 widgetRegister = smallPane $ do
     el "h2" $ do
         text "Register"
     do
-        _dynUsername <- inputBox "username" "Username" "bob" Prelude.id
-        _dynEmail <- inputBox "email" "Email Address" "bob@bob.com" Prelude.id
-        _dynName <- inputBox "name" "Name" "Bob Frog" Prelude.id
-        _dynPassword <- passwordBox "password" "Password" "abc123" Prelude.id
-        _dynPassword2 <- passwordBox "password2" "Password (again)" "abc123" Prelude.id
-        _register <- bsButton "btn btn-success" "Register"
+        _dynUsername <- divClass "my-2" $ inputBox "username" "Username" "bob" Prelude.id
+        _dynEmail <- divClass "my-2" $ inputBox "email" "Email Address" "bob@bob.com" Prelude.id
+        _dynName <- divClass "my-2" $ inputBox "name" "Name" "Bob Frog" Prelude.id
+        _dynPassword <- divClass "my-2" $ passwordBox "password" "Password" "abc123" Prelude.id
+        _dynPassword2 <- divClass "my-2" $ passwordBox "password2" "Password (again)" "abc123" Prelude.id
+        _register <- divClass "my-3" $ bsButton "btn btn-success" "Register"
         pure ()
     pure never
