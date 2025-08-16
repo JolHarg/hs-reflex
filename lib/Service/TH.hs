d0e899b2d3b9f161959720ba52a07cdd4f9f3450
Dan Dart <git@dandart.co.uk>
2025-08-16 22:33:26 +0100

    0.3.0.0

--- lib/Service/TH.hs
+++ lib/Service/TH.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell  #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Service.TH where
 
@@ -9,10 +9,10 @@ import Language.Haskell.TH
 import Servant.Reflex
 
 get ∷ Model → Q Exp
-get Model { modelName } = [|
+get Model { singularType } = [|
   client
-    (Proxy :: Proxy $(conT . mkName $ ("Get" <> modelName)))
+    (Proxy :: Proxy $(conT . mkName $ ("Get" <> singularType)))
     (Proxy :: Proxy (m :: Data.Kind.Type → Data.Kind.Type))
     (Proxy :: Proxy ())
-    (constDyn (BasePath $ "https//api.jobfinder.jolharg.com/api/" <> endpoint))
+    (constDyn (BasePath $ "https//api.jobfinder.jolharg.com/api/" <> singularEndpoint))
   |]
